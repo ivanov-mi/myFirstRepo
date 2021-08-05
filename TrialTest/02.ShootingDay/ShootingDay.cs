cf56e7540e46a9986e74a53f7a435df5961e316e
Martin Ivanov <mart0_vn@abv.bg>
2021-08-05 18:45:37 +0300

    Undo reset

--- TrialTest/02.ShootingDay/ShootingDay.cs
+++ TrialTest/02.ShootingDay/ShootingDay.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 
-
-class ShootingDay
+public class ShootingDay
 {
-    static void Main()
+    public static void Main()
     {
         int timeForShooting = int.Parse(Console.ReadLine());
         int numbersOfScenes = int.Parse(Console.ReadLine());
         int timeForAScene = int.Parse(Console.ReadLine());
 
-        if (0.85* timeForShooting - numbersOfScenes*timeForAScene >= 0)
+        if ((0.85 * timeForShooting) - (numbersOfScenes * timeForAScene) >= 0)
         {
-            Console.WriteLine("You manager to finish the movie ont time! You have {0} minutes left!", (int)Math.Round((0.85 * timeForShooting - numbersOfScenes * timeForAScene)));
+            Console.WriteLine(
+                "You manager to finish the movie ont time! You have {0} minutes left!",
+                (int)Math.Round(0.85 * timeForShooting) - (numbersOfScenes * timeForAScene));
         }
         else
         {
-            Console.WriteLine("Time is up! To complete the movie you need {0} minutes.", (int)Math.Round(Math.Abs(0.85 * timeForShooting - numbersOfScenes * timeForAScene)));
+            Console.WriteLine(
+                "Time is up! To complete the movie you need {0} minutes.",
+                (int)Math.Round(Math.Abs(0.85 * timeForShooting) - (numbersOfScenes * timeForAScene)));
         }
     }
 }
-
